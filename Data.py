473465056d6437a4ca1ccb315a9babe7909ffec5
HYBRID <48980248+hybridvamp@users.noreply.github.com>
2022-09-30 23:41:58 +0400

    hybridation

--- Data.py
+++ Data.py
@@ -7,10 +7,8 @@ class Data:
 Hey {}
 
 Welcome to {}
-
 You can use me to manage channels with tons of features. Use below buttons to learn more !
-
-By @StarkBots
+By @HYBRID_Bots
     """
 
     # Home Button
@@ -20,13 +18,13 @@ By @StarkBots
 
     # Rest Buttons
     buttons = [
-        [InlineKeyboardButton("✨ Bot Status and More Bots ✨", url="https://t.me/StarkBots/7")],
+        [InlineKeyboardButton("✨ Bot Status and More Bots ✨", url="https://t.me/HYBRID_Bots/8")],
         [
             InlineKeyboardButton("How to Use ❔", callback_data="help"),
             InlineKeyboardButton("🎪 About 🎪", callback_data="about")
         ],
-        [InlineKeyboardButton("♥ More Amazing bots ♥", url="https://t.me/StarkBots")],
-        [InlineKeyboardButton("🎨 Support Group 🎨", url="https://t.me/StarkBotsChat")],
+        [InlineKeyboardButton("♥ More Amazing bots ♥", url="https://t.me/HYBRID_Bots")],
+        [InlineKeyboardButton("🎨 Support Group 🎨", url="https://t.me/HYBRID_Chat")],
     ]
 
     # Help Message
@@ -50,13 +48,8 @@ Alternative Commands
     ABOUT = """
 **About This Bot** 
 
-A telegram channel automation bot by @StarkBots
-
-Source Code : [Click Here](https://github.com/StarkBotsIndustries/ChannelBot)
+A telegram channel automation bot by @HYBRID_Bots
 
 Framework : [Pyrogram](docs.pyrogram.org)
-
 Language : [Python](www.python.org)
-
-Developer : @StarkProgrammer
     """
